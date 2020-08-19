75dc270659ac60157aa36b1a971abcd9000017a4
Astrid Tovar <astridtovar01@gmail.com>
2020-08-18 19:29:23 -0500

    Primer commit - template listo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { dashboardRoutes } from './dashboard/dashboard.routes';
+import { LoginComponent } from './auth/login/login.component';
+import { NgModule } from '@angular/core';
+import { RegisterComponent } from './auth/register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', component: DashboardComponent, children: dashboardRoutes },
+  { path: '**', redirectTo: '' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
